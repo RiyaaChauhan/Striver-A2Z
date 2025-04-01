14b96c125e4e3b04b6f91f911feefec24c4f39d8
rcccola <riyacchauhan12@gmail.com>
2025-04-01 20:36:56 +0530

    updated

--- Day_02/_03. Implement Upper Bound.py
+++ Day_02/_03. Implement Upper Bound.py
@@ -13,8 +13,7 @@
     
 class Solution:
     def getFloorAndCeil(self, arr, x):
-        floor, ceil = -1, -1  # Default values if no floor/ceil is found
-        
+        floor, ceil = -1, -1  # Default values if no floor/ceil is found  
         for num in arr:
             if num <= x:  # Check for floor condition
                 floor = max(floor, num)
