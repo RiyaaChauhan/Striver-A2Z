c2ceb9ad3ea8341a34ffddb638a8ee8de0da2e59
rcccola <riyacchauhan12@gmail.com>
2025-04-10 09:21:26 +0530

    upated

--- Day_14/_01. Find missing number in an array.py
+++ Day_14/_01. Find missing number in an array.py
@@ -2,3 +2,18 @@ class Solution(object):
     def missingNumber(self, nums):
         n=len(nums)
         return (n*(n+1)//2-sum(nums))
+    
+          # OR 
+
+class Solution(object):
+    def missingNumber(self, nums):
+        n=len(nums)
+        xor1=0
+        xor2=0
+
+        for i in range(n + 1):
+            xor1 ^= i
+        for y in nums:
+            xor2 ^= y
+
+        return xor1 ^ xor2
