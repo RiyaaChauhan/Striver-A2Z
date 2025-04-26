cd9883477f617cf19ba1f62cc11723be02ec4e2c
Riya Chauhan <riyacchauhan123@gmail.com>
2025-04-26 11:56:50 +0530

    updated

--- Day_34/_02. Merge Two Sorted Linked Lists.py
+++ Day_34/_02. Merge Two Sorted Linked Lists.py
@@ -1,4 +1,5 @@
 # Definition for singly-linked list.
+# ITERATIVE
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -14,6 +15,33 @@ class Solution:
             else:
                 curr.next=list2
                 list2=list2.next
+            curr=curr.next
+        if list1:
+            curr.next=list1
+        if list2:
+            curr.next=list2
+        return dummy_node.next
+    
+
+    ## RECURSIVE:
+
+    # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1, list2):
+        dummy_node=ListNode(0)
+        curr=dummy_node
+        while list1 and list2:
+            if list1.val < list2.val:
+                list1.next = self.mergeTwoLists(list1.next, list2)
+                return list1
+            else:
+                list2.next = self.mergeTwoLists(list1, list2.next)
+                return list2
+
             curr=curr.next
         if list1:
             curr.next=list1
