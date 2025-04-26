4f217d469e035c02f883a4d210ec2207be6d83ce
Riya Chauhan <riyacchauhan123@gmail.com>
2025-04-26 11:58:02 +0530

    updated

--- Day_34/_02. Merge Two Sorted Linked Lists.py
+++ Day_34/_02. Merge Two Sorted Linked Lists.py
@@ -32,19 +32,14 @@ class Solution:
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1, list2):
-        dummy_node=ListNode(0)
-        curr=dummy_node
-        while list1 and list2:
-            if list1.val < list2.val:
-                list1.next = self.mergeTwoLists(list1.next, list2)
-                return list1
-            else:
-                list2.next = self.mergeTwoLists(list1, list2.next)
-                return list2
+        if not list1:
+            return list2
+        if not list2:
+            return list1
 
-            curr=curr.next
-        if list1:
-            curr.next=list1
-        if list2:
-            curr.next=list2
-        return dummy_node.next
+        if list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
