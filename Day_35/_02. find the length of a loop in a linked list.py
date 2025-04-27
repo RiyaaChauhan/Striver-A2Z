c8c47ca344c80391b02218127840b1584a4ba6b6
Riya Chauhan <riyacchauhan123@gmail.com>
2025-04-27 10:56:37 +0530

    updated

--- Day_35/_02. find the length of a loop in a linked list.py
+++ Day_35/_02. find the length of a loop in a linked list.py
@@ -20,3 +20,9 @@ def lengthOfLoop(head):
                 length += 1
             return length
     return 0  # No cycle
+'''
+Complexity:
+Time Complexity: O(n), where n is the number of nodes in the linked list. We traverse the list once to detect the cycle and once more to count the loop length.
+
+Space Complexity: O(1), as we're only using a few pointers.
+'''
